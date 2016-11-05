fe2dee186659c73cc1562fd70a28b499e4faa150
Oleksij Onysymchuk <oleksij.onysymchuk@gmail.com>
2016-11-05 12:11:34 +0200

    Changed string concat to String.fromat()

--- src/main/java/training/game/greater_less/View.java
+++ src/main/java/training/game/greater_less/View.java
@@ -2,6 +2,8 @@ package training.game.greater_less;
 
 import java.util.List;
 
+import static java.lang.String.*;
+
 /**
  * This class represents View unit of MVC based architecture of program the game "Greater/Less".
  * It contains text constants for output messages and methods to show certain blocks of game information.
@@ -17,15 +19,14 @@ public class View {
                     "out of current game range  or equals to picked number.\n" +
                     "After every try current game range becomes smaller and closer to picked value.\n" +
                     "After picked number has been guessed use's tries statistics appears and game is over.";
-    public static final String TO = " to ";
-    public static final String ENTER_INT_NUMBER_IN_RANGE_FROM = "Enter INT number in range from ";
+    public static final String ENTER_INT_NUMBER_IN_RANGE = "Enter INT number in range from %d to %d";
     public static final String RESULT_GREATER_THAN = "Your value is GREATER than picked secret number.";
     public static final String RESULT_LESS_THAN = "Your value is LESS than picked secret number.";
-    public static final String RESULT_EQUALS_TO = "CONGRATULATIONS!!! YOU HAVE BEEN GUESSED THE SECRET NUMBER = ";
+    public static final String RESULT_EQUALS_TO = "CONGRATULATIONS!!! YOU HAVE BEEN GUESSED THE SECRET NUMBER WAS %d";
     public static final String RESULT_OUT_OF_BOUNDS = "Your value is OUT OF CURRENT GAME RANGE. Pay more attention! Bounds are also not included.";
     public static final String ILLEGAL_INPUT = "You input value, which is NOT INTEGER value. You ought to enter integers only!";
-    public static final String TOTAL_TRIES_COUNT = "Your total tries count: ";
-    public static final String YOU_INPUT_VALUE_HISTORY_IS = "Your input value history is: ";
+    public static final String TOTAL_TRIES_COUNT = "Your total tries count: %d";
+    public static final String YOU_INPUT_VALUE_HISTORY_IS = "Your attempt values history is: %s";
     public static final String GAME_OVER = "GAME OVER.";
 
     public static final String HISTORY_LIST_SEPARATOR = ", ";
@@ -46,7 +47,7 @@ public class View {
      * Shows on standart out promt to enter next value
      */
     public void showPromt(int lowerBound, int upperBound) {
-        printMessage(ENTER_INT_NUMBER_IN_RANGE_FROM, lowerBound, TO, upperBound);
+        printMessage(format(ENTER_INT_NUMBER_IN_RANGE, lowerBound, upperBound));
     }
 
     /**
@@ -54,20 +55,12 @@ public class View {
      *
      * @param message
      */
-    public void showMessage(String message){
+    public void showMessage(String message) {
         printMessage(message);
     }
 
     private void printMessage(String message) {
         System.out.println(message);
-    }
-
-    private void printMessage(Object... list) {
-        StringBuilder message = new StringBuilder();
-        for (Object obj : list) {
-            message.append(obj.toString());
-        }
-        printMessage(message.toString());
     }
 
     private String convertListOfIntegerToString(List<Integer> userTries) {
@@ -82,10 +75,10 @@ public class View {
      * @param history the list of user attempts
      */
     public void showYouWinAndStatisitics(List<Integer> history) {
-        printMessage(RESULT_EQUALS_TO,history.get(history.size()-1));
+        printMessage(format(RESULT_EQUALS_TO, history.get(history.size() - 1)));
         printMessage(EMPTY_STRING);
-        printMessage(TOTAL_TRIES_COUNT, history.size());
-        printMessage(YOU_INPUT_VALUE_HISTORY_IS, convertListOfIntegerToString(history));
+        printMessage(format(TOTAL_TRIES_COUNT, history.size()));
+        printMessage(format(YOU_INPUT_VALUE_HISTORY_IS, convertListOfIntegerToString(history)));
         printMessage(EMPTY_STRING);
         printMessage(GAME_OVER);
     }
